ef8b1ccf1996a7be72a4a74fe60cc9c230a061e5
Hanus Hasl <hanus.hasl@contractors.roche.com>
2022-06-15 14:09:54 +0200

    Add commands and properties scaffolding.

--- PixelSort.App/ViewModels/MainViewModel.cs
+++ PixelSort.App/ViewModels/MainViewModel.cs
@@ -1,6 +1,62 @@
-﻿namespace PixelSort.App.ViewModels
+﻿using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using PixelSort.App.Commands;
+using PixelSort.App.ViewModels.Base;
+
+namespace PixelSort.App.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        #region Private Members
+        
+        #endregion
+
+        #region Backing Fields
+        private WriteableBitmap _writeableBitmap;
+        #endregion
+
+        #region Public Properties
+
+        public WriteableBitmap ImageDataSource
+        {
+            get => _writeableBitmap;
+            set
+            {
+                if (value != _writeableBitmap)
+                {
+                    SetProperty(ref _writeableBitmap, value);
+                }
+            }
+        }
+        #endregion
+
+        #region Constructors
+
+        public MainViewModel()
+        {
+            GenerateRandomPixelsCommand = new RelayCommand(GenerateRandomPixels, CanGenerateRandomPixels);
+        }
+        #endregion
+
+        #region Commands
+        public ICommand GenerateRandomPixelsCommand { get; }
+        #endregion
+
+        #region Command Actions
+
+        private bool CanGenerateRandomPixels(object obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void GenerateRandomPixels()
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+
+        #region Private Methods
+
+        #endregion
     }
 }
